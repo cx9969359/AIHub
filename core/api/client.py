e891240a0b3e2ce9b366fc97eb584896ff281e23
Chang <cx9969359@gmail.com>
2019-07-11 10:47:14 +0800

    no message

--- core/api/client.py
+++ core/api/client.py
@@ -1,7 +1,8 @@
 import uuid
 
-from flask import jsonify,request
+from flask import jsonify, request
 from flask_restplus import Namespace, Resource, reqparse
+from sqlalchemy.orm.exc import NoResultFound, MultipleResultsFound
 
 from core.model import ClientModel
 from core.model import db
@@ -9,8 +10,70 @@ from core.service.serialize_obj import serialize
 
 api = Namespace('client')
 
-create_client = reqparse.RequestParser()
-create_client.add_argument('name', type=str, required=True)
+init_client = reqparse.RequestParser()
+init_client.add_argument('name', type=str, required=True)
+init_client.add_argument('model_version', type=str, required=True)
+init_client.add_argument('platform_version', type=str, required=True)
+
+register_client = reqparse.RequestParser()
+register_client.add_argument('uuid', type=str, required=True)
+register_client.add_argument('LAN_IP', type=str, required=True)
+register_client.add_argument('os', type=str, required=True)
+
+
+@api.route('/init')
+class InitClient(Resource):
+    """
+    初始化一个client信息
+    """
+
+    @api.expect(init_client)
+    def post(self):
+        args = init_client.parse_args()
+        client_name = args.name
+        model_version = args.model_version
+        platform_version = args.platform_version
+
+        if ClientModel.query.filter_by(name=client_name).count() > 0:
+            msg = 'The name has existed, please entry again!'
+            return jsonify({'result': msg, 'status': 400})
+        client = ClientModel(name=client_name, uuid=uuid.uuid1(), model_version=model_version,
+                             platform_version=platform_version)
+        db.session.add(client)
+        db.session.commit()
+        return jsonify({'result': 'Create success', 'status': 200})
+
+
+@api.route('/register')
+class RegisterClient(Resource):
+    """
+    注册client信息
+    """
+
+    @api.expect(register_client)
+    def post(self):
+        args = register_client.parse_args()
+        uuid = args.uuid
+        LAN_IP = args.LAN_IP
+        os = args.os
+        public_IP = request.remote_addr
+        try:
+            client = ClientModel.query.filter_by(uuid=uuid).one()
+        except NoResultFound:
+            msg = 'The uuid is error, no such client!'
+            return jsonify({'result': msg, 'status': 400})
+        except MultipleResultsFound:
+            msg = 'Multiple result found of this uuid: {}'.format(uuid)
+            return jsonify({'result': msg, 'status': 400})
+        # 更新客户端的信息
+        client.LAN_IP = LAN_IP
+        client.public_IP = public_IP
+        client.os = os
+        db.session.commit()
+
+        # TODO 内网穿透并进行ssh_copy_id
+        
+        return jsonify({'result': '注册成功!', 'status': 200})
 
 
 @api.route('/')
@@ -22,21 +85,4 @@ class Client(Resource):
         """
         all_client = db.session.query(ClientModel).all()
         all_client = serialize(all_client)
-
         return {'result': 'success', 'client_list': all_client}
-
-    @api.expect(create_client)
-    def post(self):
-        """
-        初始化一个client
-        :return:
-        """
-        args = create_client.parse_args()
-        client_name = args.name
-        if ClientModel.query.filter_by(name=client_name).count() > 0:
-            msg = 'The name has existed, please entry again!'
-            return jsonify({'result': msg, 'status': 400})
-        client = ClientModel(name=client_name, uuid=uuid.uuid1())
-        db.session.add(client)
-        db.session.commit()
-        return jsonify({'result': 'Create success', 'status': 200})
