fb0eb9f84d9a9a95d085feea19d6ed9a95824ee4
Chang <cx9969359@gmail.com>
2019-07-11 19:48:04 +0800

    no message

--- core/api/__init__.py
+++ core/api/__init__.py
@@ -3,7 +3,7 @@ from flask_restplus import Api
 
 from core.api.client import api as client_api
 
-blueprint = Blueprint('aihub', __name__, url_prefix='/api')
+blueprint = Blueprint('aihub', __name__, url_prefix='/AIHub')
 
 api = Api(blueprint)
 api.add_namespace(client_api)
