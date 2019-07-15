734d0f89f1f77a949c5e6f4dded35db8e4785bf4
Chang <cx9969359@gmail.com>
2019-07-15 16:12:23 +0800

    no message

--- core/model.py
+++ core/model.py
@@ -3,12 +3,6 @@ from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy()
 
 
-class UserModel(db.Model):
-    __tablename__ = 'user'
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(64), unique=True)
-
-
 class ClientModel(db.Model):
     __tablename__ = 'client'
 
