a08db4255284f52f1bc3d54016ca56b2a9ecc02d
Chang <cx9969359@gmail.com>
2019-07-15 19:38:38 +0800

    no message

--- celery_app.py
+++ celery_app.py
@@ -1,3 +1,5 @@
+from __future__ import absolute_import, unicode_literals
+
 from celery import Celery
 
 from celery_package.celery_config import CeleryConfig
@@ -5,5 +7,7 @@ from celery_package.celery_config import CeleryConfig
 app = Celery('celery_package', include=['celery_package.tasks'])
 app.config_from_object(CeleryConfig)
 
-if __name__ == '__main__':
-    app.start()
+
+@app.task
+def add(x, y):
+    return x + y
--- celery_package/celery_config.py
+++ celery_package/celery_config.py
@@ -1,4 +1,6 @@
 class CeleryConfig:
-    CELERY_BROKER_URL = 'redis://127.0.0.1:6379/0'
+    BROKER_URL = 'redis://127.0.0.1:6379/0'
     CELERY_RESULT_BACKEND = 'redis://127.0.0.1:6379/1'
     CELERY_TASK_RESULT_EXPIRES = 60 * 60 * 24
+    CELERYD_CONCURRENCY = 1
+    CELERY_TIMEZONE = 'Asia/Shanghai'
